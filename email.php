64e944db8e3ca47e009bd99d9798bdea9c3593aa
Mateus Camargo <mateus.camargo@cakeerp.com>
2019-11-17 11:46:28 -0300

    Implementação da função smtpmailer

--- email.php
+++ email.php
@@ -6,20 +6,58 @@ $nome = addslashes($_POST['name']);
 $email = addslashes($_POST['email']);
 $mensagem = addslashes($_POST['message']);
 
-$to = "shp4la@tuta.io";
-$subject = "Contato - TESTE";
-$body = "Nome: ".$nome."\n".
-        "Email: ".$email."\n".
-        "Mensagem:".$mensagem;
-}
-$header = "From: mateus@vacariano.br"."\r\n".
-            "Reply-to:".$email."\r\n".
-            "X=Mailer:PHP/".phpversion();
+$send_file = "Nome: $nome \n\nE-mail: $email\n\nMensagem:\n$mensagem";
 
-if(mail($to,$subject,$body,$header)){
-    echo("Email enviado com sucesso!");
-} else {
-    echo("Email não foi enviado!");
+require_once('phpmiler/class.phpmailer.php');
+
+define('GUSER', 'martelua0@gmail.com'); // email
+define('GPDW', '------');       //senha
+
+
+function smtpmailer($para, $de, $de_nome, $assunto, $corpo) {
+
+    global $error;
+
+    $mail = new PHPMailer();
+	$mail->IsSMTP();		// Ativar SMTP
+	$mail->SMTPDebug = 0;		// Debugar: 1 = erros e mensagens, 2 = mensagens apenas
+	$mail->SMTPAuth = true;		// Autenticação ativada
+	$mail->SMTPSecure = 'ssl';	// SSL REQUERIDO pelo GMail
+	$mail->Host = 'smtp.gmail.com';	// SMTP utilizado
+	$mail->Port = 587;  		// A porta 587 deverá estar aberta em seu servidor
+	$mail->Username = GUSER;
+	$mail->Password = GPWD;
+	$mail->SetFrom($de, $de_nome);
+	$mail->Subject = $assunto;
+	$mail->Body = $corpo;
+    $mail->AddAddress($para);
+    
+
+    if(!$mail->Send()) {
+    
+        $error = 'Mail error: '.$mail->ErrorInfo; 
+		return false;
+    
+    } else {
+    
+        $error = 'Mensagem enviada!';
+		return true;
+    
+    }
+
 }
+
+$to = 'martelua0@gmail.com';
+$to_name = 'Mateus';
+$assunto = 'Teste';
+
+if (smtpmailer($email, $to, $to_name, $assunto, $send_file)) {
+
+    Header("location:http://www.google.com.br"); 
+    echo('Email enviado com sucesso!');
+    
+}
+
+if (!empty($error)) echo $error;
 
 ?>
